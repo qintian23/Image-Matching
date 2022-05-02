e4e5fbc77207fdd3609d3c618e6e5c31bbbe5500
Liucheng Zhu <13927181971@163.com>
2022-05-02 13:50:55 +0800

    SUFT

--- Matching/ORB/main.cpp
+++ Matching/ORB/main.cpp
@@ -1,6 +1,6 @@
 #include <opencv2/highgui/highgui.hpp> 
-#include <opencv2/imgproc.hpp>
 #include <opencv2/xfeatures2d/nonfree.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>  
 
 using namespace cv;
@@ -18,44 +18,37 @@ int main()
 
     //灰度图转换  
     Mat image1, image2;
-    
     cvtColor(image01, image1, COLOR_RGB2GRAY);
     cvtColor(image02, image2, COLOR_RGB2GRAY);
 
 
-    //提取特征点   
-    Ptr<FeatureDetector> OrbDetector = ORB::create();
+    //提取特征点    
+    Ptr<FeatureDetector> surfDetector = xfeatures2d::SURF::create(800);  // 海塞矩阵阈值，在这里调整精度，值越大点越少，越精准 
     vector<KeyPoint> keyPoint1, keyPoint2;
-    OrbDetector->detect(image1, keyPoint1);
-    OrbDetector->detect(image2, keyPoint2);
+    surfDetector->detect(image1, keyPoint1);
+    surfDetector->detect(image2, keyPoint2);
 
     //特征点描述，为下边的特征点匹配做准备    
-    Ptr<DescriptorExtractor> OrbDescriptor = ORB::create();
+    Ptr<DescriptorExtractor> SurfDescriptor = xfeatures2d::SURF::create(800);
     Mat imageDesc1, imageDesc2;
-    OrbDescriptor->compute(image1, keyPoint1, imageDesc1);
-    OrbDescriptor->compute(image2, keyPoint2, imageDesc2);
+    SurfDescriptor->compute(image1, keyPoint1, imageDesc1);
+    SurfDescriptor->compute(image2, keyPoint2, imageDesc2);
 
-    flann::Index flannIndex(imageDesc1, flann::LshIndexParams(12, 20, 2), cvflann::FLANN_DIST_HAMMING);
+    //获得匹配特征点，并提取最优配对     
+    FlannBasedMatcher matcher;
+    vector<DMatch> matchePoints;
 
-    vector<DMatch> GoodMatchePoints;
+    matcher.match(imageDesc1, imageDesc2, matchePoints, Mat());
+    cout << "total match points: " << matchePoints.size() << endl;
 
-    Mat macthIndex(imageDesc2.rows, 2, CV_32SC1), matchDistance(imageDesc2.rows, 2, CV_32FC1);
-    flannIndex.knnSearch(imageDesc2, macthIndex, matchDistance, 2, flann::SearchParams());
 
-    // Lowe's algorithm,获取优秀匹配点
-    for (int i = 0; i < matchDistance.rows; i++)
-    {
-        if (matchDistance.at<float>(i, 0) < 0.6 * matchDistance.at<float>(i, 1))
-        {
-            DMatch dmatches(i, macthIndex.at<int>(i, 0), matchDistance.at<float>(i, 0));
-            GoodMatchePoints.push_back(dmatches);
-        }
-    }
+    Mat img_match;
+    drawMatches(image01, keyPoint1, image02, keyPoint2, matchePoints, img_match);
+    namedWindow("match", 0);
+    imshow("match", img_match);
+    imwrite("match.jpg", img_match);
 
-    Mat first_match;
-    drawMatches(image02, keyPoint2, image01, keyPoint1, GoodMatchePoints, first_match);
-    imshow("first_match ", first_match);
-    imwrite("first_match.jpg", first_match);
     waitKey();
     return 0;
 }
+
