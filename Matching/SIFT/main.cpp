1e3371641fba7c5f1b0f7caef8a52540b1e704cf
Liucheng Zhu <13927181971@163.com>
2022-05-13 13:01:23 +0800

    sift chinese bird

--- Matching/SIFT/main.cpp
+++ Matching/SIFT/main.cpp
@@ -9,12 +9,44 @@ using namespace std;
 
 int main()
 {
-    const char* img1 = "../figure/badge.png";
-    const char* img2 = "../figure/school_name.png";
+    //const char* img1 = "../figure/badge.png";
+    //const char* img2 = "../figure/school_name.png";
+
+        // Chinese
+    //const char* img1 = "../figure/chinese/cheng.png";  // 5000,2000，对
+    //const char* img1 = "../figure/chinese/cheng1.png"; // 5000,2000，对
+    //const char* img1 = "../figure/chinese/dong.png"; // 5000,2000，对
+    //const char* img1 = "../figure/chinese/dong1.png";  // 5000,2000，对
+    //const char* img1 = "../figure/chinese/lin.png"; // 5000,2000，对
+    //const char* img1 = "../figure/chinese/lin1.png"; //  5000,2000，对
+    //const char* img1 = "../figure/chinese/wan.png";// 5000,2000，对
+    //const char* img1 = "../figure/chinese/wan1.png";// 5000,2000，对
+    //const char* img1 = "../figure/chinese/xi.png";// 5000,2000，对
+    //const char* img1 = "../figure/chinese/xi1.png";// 5000,2000，对
+    //const char* img1 = "../figure/chinese/zhang.png";// 5000,2000，对
+    //const char* img1 = "../figure/chinese/zhang1.png";// 5000,2000，对
+
+    // bird
+    //const char* img1 = "../figure/bird/bird_acc.jpg";  // 5000,2000，对
+    //const char* img1 = "../figure/bird/bird_acc_1.jpg"; // 5000,2000，对
+    //const char* img1 = "../figure/bird/bird_anianiau.jpg"; // 5000,2000，对
+    //const char* img1 = "../figure/bird/bird_anianiau_1.jpg";  // 5000,2000，对
+    //const char* img1 = "../figure/bird/bird_aoc.jpg"; // 5000,2000，对
+    //const char* img1 = "../figure/bird/bird_aoc_1.jpg"; // 5000,2000，对
+    //const char* img1 = "../figure/bird/bird_ap.jpg";// 5000,2000，对
+    //const char* img1 = "../figure/bird/bird_ap_1.jpg";// 5000,2000，对
+    //const char* img1 = "../figure/bird/bird_apapane.jpg";// 5000,2000，对
+    //const char* img1 = "../figure/bird/bird_apapane_1.jpg";// 5000,2000，对
+    //const char* img1 = "../figure/bird/bird_booby.jpg";// 5000,2000，对
+    const char* img1 = "../figure/bird/bird_booby_1.jpg";// 5000,2000，对
+
+    const char* img2 = "../figure/bird/all.png";
+    //const char* img2 = "../figure/chinese/all.png";
+
     Mat image01 = imread(img1, IMREAD_COLOR);
     Mat image02 = imread(img2, IMREAD_COLOR);
-    imshow("p2", image01);
-    imshow("p1", image02);
+    //imshow("p2", image01);
+    //imshow("p1", image02);
 
     //灰度图转换  
     Mat image1, image2;
@@ -23,40 +55,41 @@ int main()
 
 
     //提取特征点    
-    Ptr<FeatureDetector> siftDetector = SIFT::create(2000);  // 海塞矩阵阈值，在这里调整精度，值越大点越少，越精准 
+    Ptr<FeatureDetector> siftDetector = SIFT::create(5000);  // 海塞矩阵阈值，在这里调整精度，值越大点越少，越精准 
     vector<KeyPoint> keyPoint1, keyPoint2;
     siftDetector->detect(image1, keyPoint1);
     siftDetector->detect(image2, keyPoint2);
 
-    //特征点描述，为下边的特征点匹配做准备    
-    Ptr<DescriptorExtractor> SiftDescriptor = SIFT::create();
+    if (keyPoint1.size() == 0)
+    {
+        cout << "image1无法检测出特征点！！" << endl;
+        return -1;
+    }
+    if (keyPoint2.size() == 0)
+    {
+        cout << "image2无法检测出特征点！！" << endl;
+        return -1;
+    }
+
+
+    //特征点描述，为下边的特征点匹配做准备      
+    Ptr<DescriptorExtractor> SiftDescriptor = SIFT::create(2000);
     Mat imageDesc1, imageDesc2;
     SiftDescriptor->compute(image1, keyPoint1, imageDesc1);
     SiftDescriptor->compute(image2, keyPoint2, imageDesc2);
 
+    //获得匹配特征点，并提取最优配对     
     FlannBasedMatcher matcher;
-    vector<vector<DMatch> > matchePoints;
-    vector<DMatch> GoodMatchePoints;
+    vector<DMatch> matchePoints;
 
-    vector<Mat> train_desc(1, imageDesc1);
-    matcher.add(train_desc);
-    matcher.train();
-
-    matcher.knnMatch(imageDesc2, matchePoints, 2);
+    matcher.match(imageDesc1, imageDesc2, matchePoints, Mat());
     cout << "total match points: " << matchePoints.size() << endl;
 
-    // Lowe's algorithm,获取优秀匹配点
-    for (int i = 0; i < matchePoints.size(); i++)
-    {
-        if (matchePoints[i][0].distance < 0.6 * matchePoints[i][1].distance)
-        {
-            GoodMatchePoints.push_back(matchePoints[i][0]);
-        }
-    }
 
-    Mat first_match;
-    drawMatches(image02, keyPoint2, image01, keyPoint1, GoodMatchePoints, first_match);
-    imshow("first_match ", first_match);
+    Mat img_match;
+    drawMatches(image01, keyPoint1, image02, keyPoint2, matchePoints, img_match);
+
+    imshow("match", img_match);
     //imwrite("first_match.jpg", first_match);
     waitKey();
     return 0;
